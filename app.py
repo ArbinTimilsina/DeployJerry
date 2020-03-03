846868f42f254f0a6df01186555e3b780e357e85
Arbin Timilsina <arbin.timilsina@gmail.com>
2020-03-02 20:26:01 -0500

    Connecting to Heroku

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+
+from server import complete_this
+
+app = Flask(__name__)
+
+DEVICE = 'cpu'
+MODEL = GPT2LMHeadModel.from_pretrained('model')
+MODEL.to(DEVICE)
+TOKENIZER = GPT2Tokenizer.from_pretrained('model')
+
+
+@app.route('/')
+def get_seed():
+    return render_template('for_seed.html')
+
+
+@app.route('/completed', methods=['POST'])
+def make_completions():
+    if request.method == 'POST':
+        seed_sequence = request.form['seed']
+        completion = complete_this(MODEL, TOKENIZER, DEVICE, seed_sequence)
+
+        return render_template(
+            'render_this.html', seed=seed_sequence, jerry_completes=completion
+            )
+
+
+def _main(host="localhost", port=5050):
+    app.run(host=host, port=port, debug=False)
+
+
+def main():
+    """
+    The setuptools entry point.
+    """
+    _main()
---
+++ server.py
@@ -0,0 +1,44 @@
+from ftfy import fix_text
+from nltk import sent_tokenize
+
+BOS = '<|startoftext|>'
+EOS = '<|endoftext|>'
+
+
+def complete_this(
+        model, tokenizer, device, seed_sequence, max_length=40, num_sent=1
+):
+    seed_sequence = BOS + seed_sequence
+    encoded_seed_sequence = tokenizer.encode(
+        seed_sequence, add_special_tokens=False, return_tensors="pt"
+    )
+    encoded_seed_sequence = encoded_seed_sequence.to(device)
+
+    output_sequences = model.generate(
+        input_ids=encoded_seed_sequence,
+        max_length=max_length,
+        temperature=1.0,
+        top_k=0,
+        top_p=0.95,
+        repetition_penalty=1.0,
+        do_sample=True
+    )
+
+    generated_sequence = output_sequences[0].tolist()
+    decoded_generated_sequence = tokenizer.decode(
+        generated_sequence, clean_up_tokenization_spaces=True
+    )
+    generated_sequence = fix_text(decoded_generated_sequence)
+    decoded_generated_sequence = generated_sequence.replace('\n', ' ')\
+        .replace(BOS, ' ').replace(EOS, ' ').replace('</|startoftext|>', ' ')\
+        .replace('<|', ' ').replace('|>', ' ').replace('< |', ' ').replace('| >', ' ')\
+        .replace('</', ' ')
+
+    sentences = sent_tokenize(decoded_generated_sequence)
+    output = ''
+    if len(sentences) < num_sent:
+        num_sent = len(sentences)
+    for i in range(num_sent):
+        output = output + ' ' + sentences[i]
+    return output
+
